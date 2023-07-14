5ae66686daae325cebd44f4e35386d66baaba3fe
prahastakumar <prahasta.kumar@digital.ai>
2023-07-14 18:47:04 +0530

    winrm script moved to another ansible repo

--- prerequisites/windows-node/Install-WinRM.ps1
+++ prerequisites/windows-node/Install-WinRM.ps1
@@ -4,7 +4,7 @@
 # configures - firewall, and winrm settings
 #########################################################
 
-$url = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
+$url = "https://raw.githubusercontent.com/ansible/ansible-documentation/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
 $file = "$env:temp\ConfigureRemotingForAnsible.ps1"
 
 (New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
